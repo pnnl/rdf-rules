963d6d7661f033907d0a81ddd0413985398a8d27
Majid alDosari <majidaldosari-github@yahoo.com>
2026-07-30 15:50:02 -0400

    common prefixes

--- src/rdf_rules/prefixes.py
+++ src/rdf_rules/prefixes.py
@@ -1,12 +1,28 @@
-
 from .rdf import URI
-
 type = dict[str, URI]
+
+common: type = {
+"rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+"dc": "http://purl.org/dc/elements/1.1/",
+"foaf": "http://xmlns.com/foaf/0.1/",
+"rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+"xsd": "http://www.w3.org/2001/XMLSchema#",
+"owl": "http://www.w3.org/2002/07/owl#",
+"vann": "http://purl.org/vocab/vann/",
+"cc": "http://web.resource.org/cc/",
+"vs": "http://www.w3.org/2003/06/sw-vocab-status/ns#",
+"wot": "http://xmlns.com/wot/0.1/",
+"geo": "http://www.w3.org/2003/01/geo/wgs84_pos#",
+"inet": "http://www.w3.org/2001/02pd/rfc65#",
+"dca": "http://dublincore.org/2000/03/13-dcagent#",
+"uriReg": "http://www.w3.org/Addressing/schemes#",
+}
+
 def make(prefixes:type ={}, base='rdf-rules') -> type:
     _ = {'meta': f'urn:{base}:meta:',
          'data': f'urn:{base}:data:'
          }
-    _ = {**_, **prefixes,} # prefixes overrides
+    _ = {**_, **common, **prefixes,} # right overrides
     # 'sub' namespace to distinguish identifiers
     prefixes = {}
     for p, n in _.copy().items():
