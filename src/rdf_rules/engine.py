9da0f0e441f374c2b2718334269eeb317be22a38
Majid alDosari <majidaldosari-github@yahoo.com>
2026-07-31 15:28:06 -0400

    option for what data to use

--- src/rdf_rules/engine.py
+++ src/rdf_rules/engine.py
@@ -2,20 +2,22 @@ from .ontology import types as otypes
 from . import prefixes as prefixesm
 from pyoxigraph import Store
 from typing import Any
-def mkrule(a: tuple | Any):
+def mkrule(a: tuple | Any, included_data={'data', 'data-metaPO' }):
     from .rule import make as mk
     if isinstance(a, tuple):
         if len(a)>1:
             if isinstance(a[-1], dict):
                 p = a[:-1]
                 kw = a[-1]
-                return mk(*p, **kw)
+                _ = mk(*p, **kw)
             else:
-                return mk(a)
+                _ = mk(a)
         else:
-            return mk(*a)
+            _ = mk(*a)
     else:
-        return mk(a)
+        _ = mk(a)
+    _.data_and_meta_options = {'include': included_data }
+    return _
 
 
 def run(*, db = Store(),
@@ -25,6 +27,7 @@ def run(*, db = Store(),
          prefixes: prefixesm.type = {},
          use_blank_nodes: bool = False,
          infer=True, validate=True,
+         included_data = {'data', 'data-metaPO' },
          MAX_NCYCLES=10,
          log_data: bool=True, log_print: bool=True, log_debug: bool=False,
              ):
@@ -38,20 +41,21 @@ def run(*, db = Store(),
 
     # DATA LOADING
     # load each ontology in data rule
-    _ = list(mkrule(d) for d in data_rules)+[mkrule(o) for o in ontologies]
+    _ = (list(mkrule(d, included_data=included_data) for d in data_rules)
+         +[mkrule(o, included_data=included_data) for o in ontologies])
     from rdf_engine import Engine
     engine = Engine(db=db, rules=_, derand=False, MAX_NCYCLES=1, **logging)
     db = engine.run1()
 
     # RULES CYCLING
-    _ = [mkrule((o, 'inference')) for o in ontologies] if infer else []
-    _ = list(mkrule(r) for r in rules)+_
+    _ = [mkrule((o, 'inference'), included_data=included_data) for o in ontologies] if infer else []
+    _ = list(mkrule(r, included_data=included_data) for r in rules)+_
     engine = Engine(db=db,  rules=_, derand=derand, MAX_NCYCLES=MAX_NCYCLES, **logging)
     db = engine.run()
 
     # VALIDATION
     if validate:
-        _ = [mkrule((o, 'validation')) for o in ontologies]
+        _ = [mkrule((o, 'validation'), included_data=included_data) for o in ontologies]
         engine = Engine(db=db,  rules=_, derand=False, MAX_NCYCLES=1, **logging)
         db = engine.run1()
     
