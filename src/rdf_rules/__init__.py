87b253bffb2d55b50e9a03ac780503e3b3a17309
Majid alDosari <majid.aldosari@pnnl.gov>
2026-08-19 17:00:03 -0400

    access main function from pkg name

--- src/rdf_rules/__init__.py
+++ src/rdf_rules/__init__.py
@@ -2,9 +2,13 @@ from beartype.claw import beartype_this_package
 beartype_this_package()
 # conflicts with plum dispatch:?
 del beartype_this_package
-from .rule import make
+
 
 try:
     from .__version__ import version as __version__
 except:
     pass
+
+
+from .rule import make as mkrule
+from .engine import run
